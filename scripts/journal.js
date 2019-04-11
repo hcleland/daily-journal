d88d4cd8d75b95a4c6f11c4593317837e0ef7511
Heather Cleland <heathercleland@gmail.com>
2019-04-11 14:44:38 -0500

    add js file and code for adding in journal entries

---
+++ scripts/journal.js
@@ -0,0 +1,18 @@
+console.log("i'm here");
+
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+    date: "April 11, 2019",
+    conceptsCovered: "JavaScript Objects",
+    content: "Lots of words go here",
+    mood: "fine"
+}
+
+const journal1 = [];
+
+journal1.push(objectsJournalEntry);
+
+console.log(journal1);
